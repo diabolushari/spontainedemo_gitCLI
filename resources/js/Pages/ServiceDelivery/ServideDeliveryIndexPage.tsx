f95cb3379c5c58f4d4e37f69fa502d4fa0d85834
Nirmala Anish <nirmala@xocortx.com>
2024-10-21 10:13:08 +0530

    Card changes

--- resources/js/Pages/ServiceDelivery/ServideDeliveryIndexPage.tsx
+++ resources/js/Pages/ServiceDelivery/ServideDeliveryIndexPage.tsx
@@ -32,10 +32,21 @@ const ServideDeliveryIndexPage = () => {
           <div className='flex flex-col gap-5 md:flex-row'>
             <div className='grid w-full grid-cols-1 gap-3 sm:grid-cols-2 md:w-1/3'>
               <Card className=''>
-                <div className=''>test</div>
+                <div className='h1-1stop pt-5 text-center'>423</div>
+                <div className='body-1stop pb-3 pt-4 text-center'>Faulty Meters</div>
               </Card>
-              <Card className=''>
-                <div className=''>test</div>
+              <Card className='grid grid-cols-2 gap-4 p-5'>
+                <div className='flex flex-col'>
+                  <div className='h3-1stop pl-4'>76</div>
+                  <div className='small-1stop pl-4'>&lt;30d</div>
+                </div>
+                <div className='flex flex-col'>
+                  <div className='h3-1stop text-[#FCB216]'>322</div>
+                  <div className='small-1stop'>&gt;30d</div>
+                </div>
+                <div className='col-span-2 flex flex-row'>
+                  <div className='small-1stop pl-4'>Meters: Faulty Days</div>
+                </div>
               </Card>
               <Card className=''>
                 <div className=''>test</div>
