3a2a41efc4d8e3e07744a744066b0e665e856242
Swaraj Krishnan <swarajkrishnan@outlook.com>
2024-10-27 20:21:42 +0530

    export limit

--- app/Http/Controllers/DataDetail/ExportDataTableController.php
+++ app/Http/Controllers/DataDetail/ExportDataTableController.php
@@ -6,7 +6,10 @@ use App\Exports\DataTableExport;
 use App\Http\Controllers\Controller;
 use App\Models\DataDetail\DataDetail;
 use App\Services\DataTable\QueryDataTable;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Maatwebsite\Excel\Facades\Excel;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
@@ -22,8 +25,22 @@ class ExportDataTableController extends Controller implements HasMiddleware
         ];
     }
 
-    public function __invoke(DataDetail $dataDetail): BinaryFileResponse
+    public function __invoke(DataDetail $dataDetail): BinaryFileResponse|RedirectResponse
     {
+        if (! Schema::hasTable($dataDetail->table_name)) {
+            return redirect()
+                ->route('data-detail.show', $dataDetail->id)
+                ->with(['error' => 'Table '.$dataDetail->table_name.' is missing in database.']);
+        }
+
+        $noOfRows = DB::table($dataDetail->table_name)->count();
+
+        if ($noOfRows > 50000) {
+            return redirect()
+                ->route('data-detail.show', $dataDetail->id)
+                ->with(['error' => 'Table '.$dataDetail->table_name.' has more than 50,000 rows.']);
+        }
+
         return Excel::download(new DataTableExport($dataDetail, new QueryDataTable), 'data-table.xlsx');
     }
 }
