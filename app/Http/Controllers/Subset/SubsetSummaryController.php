159d4f14445a8ac6e8327f0064a2ac2be934a6e3
Swaraj Krishnan <swarajkrishnan@outlook.com>
2024-11-04 10:24:46 +0530

    subset view filter

--- app/Http/Controllers/Subset/SubsetSummaryController.php
+++ app/Http/Controllers/Subset/SubsetSummaryController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Subset;
 
 use App\Http\Controllers\Controller;
 use App\Models\Subset\SubsetDetail;
+use App\Services\Subset\SubsetFilterBuilder;
 use App\Services\Subset\SubsetQueryBuilder;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 
 class SubsetSummaryController extends Controller implements HasMiddleware
@@ -20,27 +22,47 @@ class SubsetSummaryController extends Controller implements HasMiddleware
         ];
     }
 
-    public function __invoke(SubsetDetail $subsetDetail, SubsetQueryBuilder $queryBuilder): JsonResponse
+    public function __invoke(SubsetDetail $subsetDetail,
+        SubsetQueryBuilder $queryBuilder,
+        SubsetFilterBuilder $filterBuilder,
+        Request $request): JsonResponse
     {
         $subsetDetail->load('dates.info', 'dimensions.info', 'measures.info', 'measures.weightInfo');
+
+        $filterParams = $request->all();
 
         if ($subsetDetail->group_data === 0) {
             return response()->json();
         }
 
-        $region = $queryBuilder->query($subsetDetail, true, 'region')->get();
-        $circle = $queryBuilder->query($subsetDetail, true, 'circle')->get();
-        $division = $queryBuilder->query($subsetDetail, true, 'division')->get();
-        $subdivision = $queryBuilder->query($subsetDetail, true, 'subdivision')->get();
-        $section = $queryBuilder->query($subsetDetail, true, 'section')->get();
+        $levels = [
+            'region',
+            'circle',
+            'division',
+            'subdivision',
+            'section',
+        ];
 
-        return response()->json([
-            ...$region,
-            ...$circle,
-            ...$division,
-            ...$subdivision,
-            ...$section,
-        ]);
+        $result = [];
+        foreach ($levels as $level) {
+            $query = $queryBuilder->query($subsetDetail, true, $level);
+
+            $filterBuilder->filter(
+                $query,
+                $subsetDetail,
+                $filterParams
+            );
+
+            $levelResult = $query->get();
+
+            $result = [
+                ...$result,
+                ...$levelResult,
+            ];
+
+        }
+
+        return response()->json($result);
 
     }
 }
