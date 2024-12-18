9215c82eba7cb0a03f8a317d7fbd7dc5ede787f2
Swaraj Krishnan <swarajkrishnan@outlook.com>
2024-12-18 14:57:08 +0530

    Force HTTPS for non-local environments in AppServiceProvider
    This update ensures that HTTPS and the correct root URL are enforced for environments other than the local development server. It enhances the security and consistency of URL generation across the application.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Events\ScheduledDataLoadEvent;
 use App\Listeners\ScheduledDataLoadListener;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
         //     ScheduledDataLoadEvent::class,
         //     ScheduledDataLoadListener::class
         // );
+        if (config('app.url') !== 'http://localhost:8000') {
+            //            URL::forceScheme('https');
+            URL::forceRootUrl(config('app.url'));
+        }
     }
 }
