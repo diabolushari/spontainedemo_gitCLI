d0ff48f36cb637a6f2c308a533a358cd4060207b
thejzain <thejzain@gmail.com>
2025-07-29 12:35:36 +0530

    Graphs changed to the ones used in page builder

---
+++ resources/js/Components/Charts/SampleChart/ColorPallets.tsx
@@ -0,0 +1,21 @@
+export const chartPallet = {
+  boldWarm: ['#5A0F35', '#A70028', '#FF8C1C', '#E0581F', '#0B4C5F'],
+  softNeutral: ['#E9AEB3', '#F8DED3', '#E4DFD9', '#B3C3B4', '#5D6966'],
+  freshGreen: ['#C7ECD7', '#A8DBBF', '#81CDA9', '#58B38F', '#26765C', '#0C3E2F'],
+  fireSunset: ['#030014', '#3F001E', '#730015', '#B10006', '#E53300', '#FFA100'],
+  blueGrey: ['#E1E6E9', '#29456F', '#5A89B2', '#9BCBFF', '#7E7E7E'],
+  citrusMint: ['#FFA424', '#FFC578', '#FFFFFF', '#C5F2EC', '#2EDFC3'],
+  earthGreen: ['#132E1A', '#2A4425', '#4E672B', '#7E9F41', '#EDF78E'],
+  duskContrast: ['#355C63', '#FFF7C4', '#F4C46C', '#A83F3B', '#560D15'],
+}
+
+export const graphColorPallet = {
+  boldWarm: '#5A0F35',
+  softNeutral: '#E9AEB3',
+  freshGreen: '#C7ECD7',
+  fireSunset: '#030014',
+  blueGrey: '#E1E6E9',
+  citrusMint: '#FFA424',
+  earthGreen: '#132E1A',
+  duskContrast: '#355C63',
+}
