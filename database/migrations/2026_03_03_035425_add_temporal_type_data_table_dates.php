6fe7cd091dc6eef9ff4227ae35572cfe93eff6fd
xothejzain <thej@intuonfx.com>
2026-03-03 11:09:03 +0530

    feat: add `temporal_type` enum column to `data_table_dates` table and include it in the `DataTableDate` model.

---
+++ database/migrations/2026_03_03_035425_add_temporal_type_data_table_dates.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('data_table_dates', function (Blueprint $table) {
+            $table->enum('temporal_type', ['date', 'datetime'])
+                ->default('date')
+                ->after('field_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('data_table_dates', function (Blueprint $table) {
+            $table->dropColumn('temporal_type');
+        });
+    }
+};
